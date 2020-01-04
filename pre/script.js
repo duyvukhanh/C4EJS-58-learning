3892f873bf342b7ae8cbd2b5b6d7d5a42b7f3d08
duyvukhanh <duyvukhanh@gmail.com>
2020-01-04 10:31:47 +0700

    first commit

---
+++ pre/script.js
@@ -0,0 +1,51 @@
+// let banKinh = 5;
+// const pi = 3.14;
+// let chuVi = 2 * Math.PI * banKinh;
+// chuVi = Math.round(chuVi * 100) / 100;
+// console.log(chuVi);
+
+// let me = {
+//     Name: "Vu Khanh Duy",
+//     Age: 23,
+//     familyMember: [{
+//             Father: "Vu Van Dung",
+//             Age: 55,
+//         },
+//         {
+//             Mother: "Pham Quynh Nga",
+//             Age: 47,
+//         }
+//     ]
+// }
+// console.log("Name : " + me.Name)
+// console.log("Father Name : " + me.familyMember[0].Father)
+// console.log("Mother Name : " + me.familyMember[1].Mother)
+
+// let x = 12,
+//     y = 5;
+
+// let tong = x + y;
+// let hieu = x - y;
+// let tich = x * y;
+// let thuong = x / y;
+// let du = x % y;
+// let mu = x ** y;
+
+// console.log(x + " + " + y + " = " + tong)
+// console.log(x + " - " + y + " = " + hieu)
+// console.log(x + " x " + y + " = " + tich)
+// console.log(x + " : " + y + " = " + thuong)
+// console.log(x + " : " + y + " du " + du)
+// console.log(x + "^" + y + " = " + mu)
+
+// let birthDay = new Date(1996, 5, 29)
+// let now = new(Date)
+// let Age = now.getFullYear() - birthDay.getFullYear()
+// console.log(Age)
+
+
+// random from min to max
+let min = 1;
+let max = 100;
+let rd = Math.round(Math.random() * (max - min) + min );
+console.log(rd)
